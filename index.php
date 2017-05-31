e8abe9462717b21a4697228a1210898ffbba2f43
anastasija-stepanova <nastyaaa19980@gmail.com>
2017-05-31 22:35:34 +0300

    Fonts optimized

--- index.php
+++ index.php
@@ -6,8 +6,7 @@
     <title>REEN – Made for Designers</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Lato:700,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato:700,900|Source+Sans+Pro:400,700" rel="stylesheet">
     <link rel="stylesheet" href="style/css/main.css">
   </head>
   <body>
